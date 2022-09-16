44a7b8aac69748f4d291fab3d50224daaaf55191
Julian <julian.schmidt@axiomait.com>
2022-09-16 00:14:52 -0300

    first commit

---
+++ TP_1/src/TP_1.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : TP_1.c
+ Author      : Julián Schmidt
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts(":3"); /* prints :3 */
+	return EXIT_SUCCESS;
+
+}
